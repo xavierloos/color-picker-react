e798c3f6ea73d3308509c56715e2146d86718c68
Javier Lopez <javierlopez@MacBook-Pro.home>
2020-08-09 17:54:07 +0100

    Adding components

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Random from './Components/Random';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Random />
+    </div>
+  );
+}
+
+export default App;
